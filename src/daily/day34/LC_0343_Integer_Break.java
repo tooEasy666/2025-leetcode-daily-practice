3dfd715b7e10f1b4a75f5710c55e61eb4caa8f9e
tooEasy666 <215785458+tooEasy666@users.noreply.github.com>
2025-07-13 21:15:19 -0700

    add comments

--- src/daily/day34/LC_0343_Integer_Break.java
+++ src/daily/day34/LC_0343_Integer_Break.java
@@ -7,8 +7,8 @@ public class LC_0343_Integer_Break {
         int timesOf3 = n / 3;
         int remainder = n % 3;
 
-        if (remainder == 0) return (int) Math.pow(3, timesOf3);
-        if (remainder == 1) return (int) Math.pow(3, timesOf3 - 1) * 4;
-        return (int) Math.pow(3, timesOf3) * 2;
+        if (remainder == 0) return (int) Math.pow(3, timesOf3); // 最多能拆多少个 3
+        if (remainder == 1) return (int) Math.pow(3, timesOf3 - 1) * 4; // 拆完 3 后	拿掉一个 3 换成 2 + 2 → 4
+        return (int) Math.pow(3, timesOf3) * 2; // 	拆完 3 后直接加个 2 - 余数为2
     }
 }
